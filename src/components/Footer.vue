80a336a6b2f459d7d1497057ec109f20603e6761
benedictamoako <amoakobenedict@gmail.com>
2023-04-03 02:23:35 +0000

    v1

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <footer>
+        <p>Copyright &copy; 2023</p>
+        <router-link v-show="aboutPage" to="/about">About</router-link>
+    </footer>
+</template>
+
+
+<script>
+export default {
+    name: 'Footer',
+    computed: {
+        aboutPage() {
+            if(this.$route.path !== '/about') {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+a {
+    color: #333;
+}
+
+footer {
+    margin-top: 30px;
+    text-align: center;
+}
+</style>
